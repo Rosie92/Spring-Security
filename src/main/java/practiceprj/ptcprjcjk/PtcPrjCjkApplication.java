83b6635575a80909d5c8903b0514f44623ff5e7f
rosie92 <107wjd@naver.com>
2022-10-05 14:41:59 +0900

    프로젝트 생성 및 DB 연동

---
+++ src/main/java/practiceprj/ptcprjcjk/PtcPrjCjkApplication.java
@@ -0,0 +1,13 @@
+package practiceprj.ptcprjcjk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtcPrjCjkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtcPrjCjkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/practiceprj/ptcprjcjk/PtcPrjCjkApplicationTests.java
@@ -0,0 +1,13 @@
+package practiceprj.ptcprjcjk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtcPrjCjkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
